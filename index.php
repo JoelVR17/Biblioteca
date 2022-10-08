7b241eefe4f30f73760753a885a76b091f79a0a8
JoelVR17 <vermudo.com@gmail.com>
2022-10-07 20:55:15 -0600

    Se agregan los archivos globales del SASS

--- index.php
+++ index.php
@@ -5,27 +5,49 @@
 <html lang="en">
 
 <!-- HEAD -->
+
 <head>
+    <!-- META -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- LINKS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="./css/styles.css">
+    <!-- FONTS -->
+    <style>
+        /* Monserrat */
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@500&family=Permanent+Marker&display=swap');
+        /* Roboto */
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap');
+        /* Open Sans */
+        @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&display=swap');
+        /* Michroma */
+        @import url('https://fonts.googleapis.com/css2?family=Michroma&display=swap');
+    </style>
+    <!-- TITLE -->
     <title>Biblioteca Joel Vargas</title>
-</head> <!--fin.Head-->
+</head>
+<!--fin.Head-->
 
 <!-- BODY -->
 <body>
 
     <!-- MAIN -->
-    <main class="contenedor">
-        
-    </main> <!--fin.main-->
+    <main class="container">
+        <h1>Hola</h1>
+        <h2>Hola</h2>
+        <h3>Hola</h3>
+        <h4>Hola</h4>
+    </main>
+    <!--fin.main-->
 
-</body> <!--fin.body-->
+</body>
+<!--fin.body-->
 
 <!-- JAVASCRIPT -->
 <script src=""></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
-</html> <!--fin.html-->
+</html>
+<!--fin.html-->
